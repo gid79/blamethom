276547d0dd6f2319f6c78e886a07298a42a4d3fe
Gareth Davis <gadavis@expedia.com>
2013-11-26 10:16:33 +0000

    allow blaming of other people for the anything

--- tube_sign.rb
+++ tube_sign.rb
@@ -2,7 +2,9 @@ require 'sinatra'
 require 'RMagick'
 include Magick
 
+valid_names = ['Any one','Everyone','Thom','Gareth','Dave','Dan','Andy','Steve']
 get '/' do
+  @names = valid_names
   erb :index
 end
 
@@ -19,20 +21,31 @@ end
 
 get '/image.png' do
 
-  text = ['Thom','is blamed'] + params[:text].values.delete_if {|a|a.empty?}
+  who = params[:who]
+  # default to thom if null or invalid
+  who = 'Thom' if who.nil? 
+  who = 'Thom' unless valid_names.include? who
+  
+  
+  text = [who,'is blamed for'] + params[:text].values.delete_if {|a|a.empty?}
 
-  filename = File.join("public/cache", Digest::MD5.hexdigest(params.to_s)+".png")
+  filename = File.join("public/cache", Digest::MD5.hexdigest(text.join('').to_s)+".png")
 
   reenie_beanie ='fonts/Reenie_Beanie/ReenieBeanie.ttf'
   gloria = 'fonts/Gloria_Hallelujah/GloriaHallelujah.ttf'
   pointsize_base = 50
 
   unless File.exists? filename
+    # note the randomisation ia post hashing of file name
+    if who == 'Any one'
+      text[0] = (valid_names[1..-1] + ['Thom'] * 30).sample # a little skewed maybe
+    end
+    
     # create image
     img = ImageList.new("thom_is_blamed.png") 
     draw = Draw.new
   
-    y = 250
+    y = 150
 
     text.each_with_index do | t, i |
       pointsize = if i == 0 
@@ -43,7 +56,7 @@ get '/image.png' do
 
       y = y+60
       if i == 0
-        draw.annotate(img,0,0,100 -4, y-4, t) {
+        draw.annotate(img,0,0,100 -3, y-3, t) {
           self.font = reenie_beanie
           self.fill = '#F53842AA'
           self.stroke = 'transparent'
